36888ea861df3d7e290321cef7bb29459172ca2c
Snowden254 <gacherupita@gmail.com>
2023-08-13 11:09:05 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - prints all single digit numbers of base 10 starting from 0 followed by a new line
  *
- * Return: always 0
+ * Return: always 0 (success)
  */
 
 int main(void)

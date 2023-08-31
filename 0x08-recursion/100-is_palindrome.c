f8705d38b808a3eda18d56dc3ee81997e52150ea
Snowden254 <gacherupita@gmail.com>
2023-08-30 18:11:15 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * last_index -returns the last index of a string {counts the null char}
+ * @s: pointer to the string
+ * Return: int
+ */
+int is_palindrome(char *s);
 int check(char *s, int start, int end, int mod);
 int last_index(char *s)
 {
